c887dd8f1dce57f73a83a43a87d6299df5dface3
Manan Arora <mananarora313@gmail.com>
2024-08-19 17:49:35 -0500

    updated NotFoundPage to include NavBar

--- codalysis/src/pages/NotFoundPage.jsx
+++ codalysis/src/pages/NotFoundPage.jsx
@@ -1,21 +1,24 @@
-
 import { Link } from "react-router-dom";
 import { FaExclamationTriangle } from "react-icons/fa";
+import Navbar from "../components/Navbar";
 
 const NotFoundPage = () => {
   return (
-    <section className="text-center flex flex-col justify-center items-center h-96">
-      <FaExclamationTriangle className="text-yellow-400 text-6xl mb-4"></FaExclamationTriangle>
-      <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
-      <p className="text-xl mb-5">This page does not exist</p>
-      <Link
-        to="/"
-        className="text-white bg-black hover:bg-slate-500 rounded-md px-3 py-2 mt-4"
-      >
-        Go Back
-      </Link>
-    </section>
-  )
-}
+    <>
+      <Navbar />
+      <section className="text-center flex flex-col justify-center items-center h-96">
+        <FaExclamationTriangle className="text-yellow-400 text-6xl mb-4"></FaExclamationTriangle>
+        <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
+        <p className="text-xl mb-5">This page does not exist</p>
+        <Link
+          to="/"
+          className="text-white bg-black hover:bg-slate-500 rounded-md px-3 py-2 mt-4"
+        >
+          Go Back
+        </Link>
+      </section>
+    </>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
